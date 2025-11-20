670f565820acdf2dbebdda6f34bc05a6c688d738
houda-bai <houda.baidane@univ-tlse3.fr>
2025-11-21 00:25:48 +0100

    duesl entre Ronin et Yakuza

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -3,6 +3,7 @@ package histoire;
 import personnages.Humain;
 import personnages.Commerçant;
 import personnages.Yakuza;
+import personnages.Ronin;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
@@ -25,9 +26,20 @@ public class HistoireTP4 {
         
         System.out.println();
       //yazuka
-      		Yakuza yaku = new Yakuza("Yaku Le Noir","whisky",30,"Warsong");
-      		yaku.direBonjour();
-      		yaku.extorqer(marco);
+      		//Yakuza yaku = new Yakuza("Yaku Le Noir","whisky",30,"Warsong");
+      		//yaku.direBonjour();
+      		//yaku.extorqer(marco);
+      	System.out.println();
+      //Ronin 
+      		//Ronin roro=new Ronin("Roro","shouchu",60);
+      		//roro.direBonjour();
+      		//roro.donner(marco);
+      		
+      //roro yaku 
+      		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 45, "Warsong");
+      	    Ronin roro = new Ronin("Roro", "shochu", 60);
+      	    roro.donner(marco);
+      	    roro.provoquer(yaku);
 	}
 		
 }
--- src/personnages/Yakuza.java
+++ src/personnages/Yakuza.java
@@ -1,21 +1,39 @@
 package personnages;
 
-public class Yakuza  extends Humain{
-	 private String clan;
-	 private int reputation;
-	 
-	 public Yakuza(String nom, String boissonFavorite, int argent, String clan) {
-	        super(nom, boissonFavorite, argent);
-	        this.clan = clan;
-	        this.reputation = 0;
+public class Yakuza extends Humain {
+	private String clan;
+	private int reputation;
+
+	public Yakuza(String nom, String boissonFavorite, int argent, String clan) {
+		super(nom, boissonFavorite, argent);
+		this.clan = clan;
+		this.reputation = 0;
+	}
+
+	public void extorqer(Commerçant victime) {
+		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par la ?");
+		parler(victime.getNom() + ",si tu tiens a la vie donne moi ta bourse !");
+		int argentpris = victime.seFaireExtorquer();
+		gagnerArgent(argentpris);
+		reputation++;
+		parler("J'ai pique les " + argentpris + " sous de " + victime.getNom() + ", ce qui me fait "
+				+ getQuantiteArgent() + " sous dans ma poche. Hi ! Hi !");
+	}
+
+	public int perdre() {
+        int perdu = getQuantiteArgent();
+        perdreArgent(perdu);
+        reputation--;
+        parler("J'ai perdu mon duel et mes " + perdu + " sous, snif... J'ai deshonore le clan de " + clan + ".");
+        return perdu;
+    }
+	 public void gagner(int gain) {
+	        gagnerArgent(gain);
+	        reputation++;
+	        parler("Ce duel m'a rapporte " + gain + " sous ! Je m'enrichis et ma reputation monte.");
 	 }
 	 
-	 public void extorqer(Commerçant victime) {
-		parler("Tiens, tiens, ne serait-ce pas un faible marchand qui passe par la ?");
-		parler(victime.getNom()+ ",si tu tiens a la vie donne moi ta bourse !");
-		int argentpris = victime.seFaireExtorquer(); 
-        gagnerArgent(argentpris);
-        reputation++;
-        parler("J'ai pique les " + argentpris + " sous de " + victime.getNom() + ", ce qui me fait " + getQuantiteArgent()+ " sous dans ma poche. Hi ! Hi !");
+	 public int getReputation() {
+	        return reputation;
 	 }
 }
